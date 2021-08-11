b8f475e9fa97cd6b1fafcd7b5644b96120406c7a
CD Yang <cdyang1991@gmail.com>
2021-08-11 23:24:34 +0800

    format python file

--- wasmServer.py
+++ wasmServer.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
-import BaseHTTPServer, SimpleHTTPServer
- 
-port=8000
+import BaseHTTPServer
+import SimpleHTTPServer
+
+port = 8000
 print "Running on port %d" % port
- 
+
 SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.wasm'] = 'application/wasm'
- 
+
 httpd = BaseHTTPServer.HTTPServer(('localhost', port),
-    SimpleHTTPServer.SimpleHTTPRequestHandler)
- 
+                                  SimpleHTTPServer.SimpleHTTPRequestHandler)
+
 print "Mapping \".wasm\" to \"%s\"" % SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map['.wasm']
 httpd.serve_forever()
